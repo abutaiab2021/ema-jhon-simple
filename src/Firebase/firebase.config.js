a303e480ca7773650040839368a392f102b1d589
abutaiab2021 <abutaiabcse95@gmail.com>
2021-10-26 22:21:27 +0600

    added with back end

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,20 @@
+// console.log(process.env);
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCLG59Msz-i3lc0hV4FZQiDCCbdHssNgso",
-    authDomain: "ema-john-simple-f9043.firebaseapp.com",
-    projectId: "ema-john-simple-f9043",
-    storageBucket: "ema-john-simple-f9043.appspot.com",
-    messagingSenderId: "253100013892",
-    appId: "1:253100013892:web:63a42dca7c06cd5bb73f11"
-  };
+  apiKey: "AIzaSyCLG59Msz-i3lc0hV4FZQiDCCbdHssNgso",
+  authDomain: "ema-john-simple-f9043.firebaseapp.com",
+  projectId: "ema-john-simple-f9043",
+  storageBucket: "ema-john-simple-f9043.appspot.com",
+  messagingSenderId: "253100013892",
+  appId: "1:253100013892:web:63a42dca7c06cd5bb73f11"
+};
 
 export default firebaseConfig;
